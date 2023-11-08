837647870009732e41dbbf9a0d5961ad426d421a
rubennmg <rubenmg2015@gmail.com>
2023-11-08 01:17:43 +0100

    ideas

---
+++ challenge01/challenge01.py
@@ -0,0 +1,30 @@
+f = open('challenge01.txt', 'r')
+
+# Leer palabras del fichero
+# Almacenar palabras leidas en una lista
+# Si no está en la lista, añadirla
+# Contar cuantas veces aparece la palabra (para cada palabra)
+# Mostrar por pantalla las palabras y el número de veces que aparecen
+# Ordenar la lista de palabras por orden en que aparecen las palabras
+
+# # Leer palabras del fichero
+# words = f.read().split()
+
+# # Almacenar palabras leidas en una lista
+# words_list = []
+
+# # Si no está en la lista, añadirla
+# for word in words:
+#     if word not in words_list:
+#         words_list.append(word)
+
+# # Contar cuantas veces aparece la palabra (para cada palabra)
+# for word in words_list:
+#     print(word, words.count(word))
+
+
+# # Mostrar por pantalla las palabras y el número de veces que aparecen
+# # Ordenar la lista de palabras por orden en que aparecen las palabras
+
+# # Cerrar fichero
+# f.close()

f634f8427d210c665292584140f1b5dea7f03d58
rubennmg <UO282651@uniovi.es>
2023-11-12 18:05:15 +0100

    trying implementation on c

---
+++ challenge02/challenge02.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+    FILE *file;
+    int i = 0;
+    char c;
+    char* result = NULL;
+
+    file = fopen("message02.txt", "r");
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    result = malloc(sizeof(char));
+    if (result == NULL)
+    {
+        printf("Error allocating memory\n");
+        fclose(file);
+        return 1;
+    } 
+
+    while (1)
+    {
+        printf("%c", c);
+        switch (c)
+        {
+            case '#':
+                i += 1;
+                break;
+            case '@':
+                i -= 1;
+                break;
+            case '*':
+                i *= i;
+                break;
+            case '$':
+                {
+                    char temp[sizeof(int)];
+                    snprintf(temp, sizeof(temp), "%d", i);
+                    
+                    result = realloc(result, strlen(result) + strlen(temp) + 1);    
+                    if (result == NULL)
+                    {
+                        printf("Error reallocating memory\n");
+                        fclose(file);
+                        return 1;
+                    }
+
+                    strncat(result, temp, sizeof(int));
+                }
+                break;
+        }
+    }
+
+    fclose(file);
+    printf("%s\n", result);
+    free(result);
+
+    return 0;
+}
